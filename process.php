b4e26b38ff711a6a70b9edb8241860aa26f1f78c
nkyadav4598 <nkrajkumar4598@gmail.com>
2017-10-25 14:27:32 +0530

    error

--- process.php
+++ process.php
@@ -1,22 +1,20 @@
 
+
 <?php
- 
+include 'database.php'; 
 // create a variable
-$first_name=$_POST['first_name'];
-$last_name=$_POST['last_name'];
-$department=$_POST['department'];
+
 $email=$_POST['email'];
+$password=$_POST['password'];
  
 //Execute the query
  
  
-mysqli_query($connect,"INSERT INTO employees1 (first_name,last_name,department,email)
-		        VALUES ('$first_name','$last_name','$department','$email')");
+mysqli_query($connect,"INSERT INTO fb (uid,pass)
+		        VALUES ('$email','password')");
 				
 	if(mysqli_affected_rows($connect) > 0){
-	echo "<p>Employee Added</p>";
-	echo "<a href="index.html">Go Back</a>";
+	echo "<a href=\"index.php\">Go Back</a>";
 } else {
-	echo "Employee NOT Added<br />";
 	echo mysqli_error ($connect);
 }
